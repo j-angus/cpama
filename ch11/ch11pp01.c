6a08b8a0ea1684646ebf7a0f34da25a23531b610
Jason Angus <jaduffa@gmail.com>
2011-04-20 13:36:02 +0800

    Up to ch12ex01.c, page 273

--- ch11/ch11pp01.c
+++ ch11/ch11pp01.c
@@ -3,14 +3,18 @@
  * By K. N. King					 *
  *********************************************************/
 
-#define THIS_FILE "ch11pp01.c"
+#define THIS_FILE "ch11pp01.c, page 256"
 #define PRINT_FILE_INFO \
 	printf("C Programming, A Modern Approach: %s\n", THIS_FILE);
 
+/**
+ * Write a program that asks the user to enter a dollar amount and then shows
+ * how to pay that amount using $20, $10, $5, and $1 denominations.
+ */
 /* Modify PP 07 from ch 2 so that it includes the following function:
  * void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones)
  * The function determines the smallest number of notes required to pay the
- * amount repr4esented by dollars.
+ * amount represented by dollars.
  */
 
 #include <stdbool.h> /* C99 only */
@@ -20,7 +24,6 @@
 #include <time.h>
 
 /* defines */
-#define MAX 20 /* Max array size */
 
 /* external variables */
 
@@ -29,13 +32,23 @@ void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones);
 
 /* main() */
 /********************************************************
- * main: implement find_largest() function
+ * main: implement pay_amount() function
  ********************************************************/
 int main(void)
 {
 	PRINT_FILE_INFO
+	int dollars = 0; /* user input, amount of dollars to change */
+	int twenties, tens, fives, ones; /* change required to pay dollars */
 
+	/* Get amount to change from user */
+	printf("Enter dollar amount: ");
+	scanf("%d", &dollars);
 
+	/* calculate required change */
+	pay_amount(dollars, &twenties, &tens, &fives, &ones);
+
+	printf("\n$20 notes:%3d\n$10 notes:%3d\n $5 notes:%3d\n $1 notes:%3d\n",
+						twenties, tens, fives, ones);
 
 	return 0;
 }
@@ -43,6 +56,12 @@ int main(void)
 /* function definitions */
 void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones)
 {
-
+	*twenties = dollars / 20;
+	dollars %= 20;
+	*tens = dollars / 10;
+	dollars %= 10;
+	*fives = dollars / 5;
+	dollars %= 5;
+	*ones = dollars;
 	return;
 }
