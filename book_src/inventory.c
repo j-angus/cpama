f637ba72b8151e8769137808cb2995cdf7fba592
Jason Angus <jaduffa@gmail.com>
2011-06-05 08:42:29 +0800

    Finished ch17pp01, page 456

--- book_src/inventory.c
+++ book_src/inventory.c
@@ -1,13 +1,9 @@
 /*********************************************************
  * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
  * By K. N. King                                         *
- * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
- * All rights reserved.                                  *
- * This program may be freely distributed for class use, *
- * provided that this copyright notice is retained.      *
  *********************************************************/
-
-/* inventory.c (Chapter 16, page 391) */
+.
+/* Based on inventory.c (Chapter 16, page 391) */
 /* Maintains a parts database (array version) */
 
 #include <stdio.h>
