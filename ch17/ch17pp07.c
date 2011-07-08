752d168b2dd23421c5b69eed1ec8df280cbe39e7
Jason Angus <jaduffa@gmail.com>
2011-07-08 18:26:57 +0800

    change spaces in source file to tabs

--- ch17/ch17pp07.c
+++ ch17/ch17pp07.c
@@ -1,6 +1,6 @@
 /*********************************************************
  * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
- * By K. N. King                                         *
+ * By K. N. King					 *
  *********************************************************/
 /* ch17pp07.c */
 /**
@@ -14,8 +14,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_REMIND 50   /* maximum number of reminders */
-#define MSG_LEN 60      /* max length of reminder message */
+#define MAX_REMIND 50	/* maximum number of reminders */
+#define MSG_LEN 60	/* max length of reminder message */
 
 struct vstring {
 	int len;
@@ -29,62 +29,63 @@ int main(void)
 {
 	struct vstring *reminders[MAX_REMIND];
 /*
-  char *reminders[MAX_REMIND];
+	char *reminders[MAX_REMIND];
 */
-  char day_str[3], msg_str[MSG_LEN+1];
-  int day, i, j, num_remind = 0;
+	char day_str[3], msg_str[MSG_LEN+1];
+	int day, i, j, num_remind = 0;
 
-  for (;;) {
-    if (num_remind == MAX_REMIND) {
-      printf("-- No space left --\n");
-      break;
-    }
+	for (;;) {
+		if (num_remind == MAX_REMIND) {
+			printf("-- No space left --\n");
+			break;
+		}
 
-    printf("Enter day and reminder: ");
-    scanf("%2d", &day);
-    if (day == 0)
-      break;
-    sprintf(day_str, "%2d", day);
-    read_line(msg_str, MSG_LEN);
+		printf("Enter day and reminder: ");
+		scanf("%2d", &day);
+		if (day == 0)
+			break;
+		sprintf(day_str, "%2d", day);
+		read_line(msg_str, MSG_LEN);
 
-    for (i = 0; i < num_remind; i++)
-      /*
-      if (strcmp(day_str, reminders[i]) < 0)
-      */
-      if (strcmp(day_str, reminders[i]->chars) < 0)
-        break;
-    for (j = num_remind; j > i; j--)
-      reminders[j] = reminders[j-1];
+		for (i = 0; i < num_remind; i++)
+			/*
+			if (strcmp(day_str, reminders[i]) < 0)
+			*/
+			if (strcmp(day_str, reminders[i]->chars) < 0)
+				break;
+		for (j = num_remind; j > i; j--)
+			reminders[j] = reminders[j-1];
 /*
-    reminders[i] = malloc(2 + strlen(msg_str) + 1);
-    */
-    reminders[i] = malloc(sizeof(struct vstring) + 2 + strlen(msg_str) + 1);
-    if (reminders[i] == NULL) {
-      printf("-- No space left --\n");
-      break;
-    }
-    reminders[i]->len = 2 + strlen(msg_str) + 1;
+		reminders[i] = malloc(2 + strlen(msg_str) + 1);
+*/
+		reminders[i] = malloc(sizeof(struct vstring) +
+						2 + strlen(msg_str) + 1);
+		if (reminders[i] == NULL) {
+			printf("-- No space left --\n");
+			break;
+		}
+		reminders[i]->len = 2 + strlen(msg_str) + 1;
 
-    strcpy(reminders[i]->chars, day_str);
-    strcat(reminders[i]->chars, msg_str);
+		strcpy(reminders[i]->chars, day_str);
+		strcat(reminders[i]->chars, msg_str);
 
-    num_remind++;
-  }
+		num_remind++;
+	}
 
-  printf("\nDay Reminder\n");
-  for (i = 0; i < num_remind; i++)
-    printf(" %s\n", reminders[i]->chars);
+	printf("\nDay Reminder\n");
+	for (i = 0; i < num_remind; i++)
+		printf(" %s\n", reminders[i]->chars);
 
-  return 0;
+	return 0;
 }
 
 int read_line(char str[], int n)
 {
-  int ch, i = 0;
+	int ch, i = 0;
 
-  while ((ch = getchar()) != '\n')
-    if (i < n)
-      str[i++] = ch;
-  str[i] = '\0';
-  return i;
+	while ((ch = getchar()) != '\n')
+		if (i < n)
+			str[i++] = ch;
+	str[i] = '\0';
+	return i;
 }
